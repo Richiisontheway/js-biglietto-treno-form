c1c444e3b05c21ad87eb8ae4055f9b860c047dc4
Richiisontheway <mineiriccardo@gmail.com>
2023-12-04 17:09:33 +0100

    esercizio finito

--- js/scripts.js
+++ js/scripts.js
@@ -6,52 +6,48 @@ genera.addEventListener('click',function() {
     const nameInput = document.getElementById('namesurname');
     console.log('nameInput.value',nameInput.value,typeof nameInput.value);
 
-    const kmInput = document.getElementById('distance')
-    console.log('kmInput',kmInput.value,typeof kmInput.value)
+    const kmInput = parseInt(document.getElementById('distance').value);
+    console.log('kmInput',kmInput,typeof kmInput)
 
-    const bornInput = document.getElementById('born')
-    console.log('bornInput',bornInput.value,typeof bornInput.value)
+    const bornInput = parseInt(document.getElementById('born').value)
+    console.log('bornInput',bornInput,typeof bornInput)
+
+    let priceKm = ('0.21')
+
+    let price = (priceKm * kmInput)
+    console.log('price',price,typeof price)
+
+    let age
+    let currentTime = ('2023')
+    age = (currentTime - bornInput)
+    console.log('age',age,typeof age)
+
+    if (age < 18){
+        price = (price * 0.8)
+        console.log('Prezzo Minorenni',price,typeof price)
+    }
+    if (age > 65){
+        price = (price * 0.6)
+        console.log('Prezzo Over 65',price,typeof price)
+    }
+
+    let moneta = ('€');
+    
+    const nation = document.getElementById('nation').value;
+    if(nation === 'asian'){
+
+        moneta = ('yen')
+        price = (price * 158.94)
+        console.log('yen',price,typeof price)
+    }
+    else if(nation === 'americian'){
+
+        moneta = ('$')
+        price = (price * 1.08)
+        console.log('$',price,typeof price)
+    }
+
+    const pricePrint = price.toFixed(2) + moneta;
+    document.getElementById('final-Price').innerHTML = pricePrint;
+    
 });
-
-
-// const km = prompt ('quanti km vuoi percorrere')
-
-// console.log('km',KilometriDaPercorrere)
-
-// if (isNaN(KilometriDaPercorrere)) {
-//     alert('Scrivi un Numero')
-// }
-
-// //Moneta
-
-// const Moneta = ('€')
-
-// //Prezzo
-
-// let Prezzo = (0.21 * KilometriDaPercorrere)
-
-// console.log('prezzo',Prezzo + Moneta)
-
-// //Età
-
-// const Age = prompt ('quanti anni hai')
-// console.log('età',Age)
-
-// //Calcolo finale
-
-
-// if (isNaN(Age)) {
-//     alert('Scrivi un Numero')
-// }
-// if(Age > 65){
-//     Prezzo = (Prezzo * 0.6)
-//     console.log('Per Vecchi',Prezzo)
-// }
-// if(Age < 18){
-//     Prezzo = (Prezzo * 0.8)
-//     console.log('Per Giovini',Prezzo)
-// }
-
-// //stamp su html
-// const pricePrint = Prezzo.toFixed(2) + Moneta;
-// document.getElementById ("prezzo-biglietto").innerHTML = pricePrint
